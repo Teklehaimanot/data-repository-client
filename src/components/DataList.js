383acad56185f0da1c4beca87bef90979fae6c4a
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-06-01 12:25:50 +0300

    building ui for creating dataset

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { BASE_URL } from '../base';
 import DataSetTitle from './DataSetTitle';
 import ListItem from './ListItem';
+import SearchBar from './SearchBar';
 
 const DataList = () => {
   const url = BASE_URL;
@@ -13,8 +14,7 @@ const DataList = () => {
       .get(`${url}/api/dataset`, {
         headers: {
           'Content-Type': 'application/json',
-          'x-auth':
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjhjODEyZjdlYjIxNzNkOTRkYWMzNmYiLCJ1c2VybmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsIm5hbWUiOiJUZWtsZWhhaW1hbm90IiwiaWF0IjoxNjUzOTM5Mjk0fQ.JS-WDqDWzI1zcOchS5C40l2TfI8vY_QytsW7cOYeczg',
+          'x-auth': `${localStorage.getItem('token')}`,
         },
       })
       .then((res) => {
@@ -23,16 +23,122 @@ const DataList = () => {
   }, []);
   return (
     <div className="w-5/6  shadow-2xl sm:rounded-lg float-right ">
-      <table className="w-full text-left ">
-        <thead className=" text-xl text-primary bg-secondary ">
-          <DataSetTitle />
-        </thead>
-        <tbody>
-          {dataset.map((data) => (
-            <ListItem key={data._id} item={data} />
-          ))}
-        </tbody>
-      </table>
+      <SearchBar />
+      {false ? (
+        <table className="w-full text-left ">
+          <thead className=" text-xl text-primary bg-secondary ">
+            <DataSetTitle />
+          </thead>
+          <tbody>
+            {dataset.map((data) => (
+              <ListItem key={data._id} item={data} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="mx-20 my-10 bg-secondary rounded-lg shadow-xl ">
+          <form className="flex flex-wrap justify-between " action="">
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex flex-col p-4 w-1/3 shadow-xl my-10">
+              <label className="text-primary text-2xl" htmlFor="">
+                Dataset Name
+              </label>
+              <input
+                type="text"
+                className=" p-2 border border-primary text-xl rounded-lg focus:blue focus:border-blue"
+              />
+            </div>
+            <div className="flex px-5 my-20">
+              <button
+                type="submit"
+                className="border hover:bg-blue hover:text-secondary px-5 py-5 border-primary text-2xl rounded-lg focus:blue focus:border-blue"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
